1f44d8e64d98c0359b67def7a1d92ed2a750d220
tokki92 <pyw5420@naver.com>
2022-01-13 13:25:05 +0900

    엔티티 클래스 정의

---
+++ src/main/java/com/tokki92/mreview/entity/MovieImage.java
@@ -0,0 +1,28 @@
+package com.tokki92.mreview.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@ToString(exclude = "movie")
+public class MovieImage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long inum;
+
+    private String uuid;
+
+    private String imgName;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Movie movie;
+
+}
