e69c1452704b4900386f2fbf31dbe54652b664e2
tokki92 <pyw5420@naver.com>
2022-01-13 17:37:50 +0900

    파일 업로드 처리 예제

---
+++ src/main/java/com/tokki92/mreview/controller/UploadTestController.java
@@ -0,0 +1,13 @@
+package com.tokki92.mreview.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UploadTestController {
+
+    @GetMapping("/uploadEx")
+    public void uploadEx() {
+
+    }
+}
